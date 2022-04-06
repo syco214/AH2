ce89e8d58f195b2840d15618ab2ec77107cfc9b3
syco214 <51996848+syco214@users.noreply.github.com>
2022-04-06 20:48:51 +0800

    first commit

---
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Menu, Modal } from 'antd';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { Notifications } from '../Notifications';
+import useWindowDimensions from '../../utils/layout';
+import { MenuOutlined } from '@ant-design/icons';
+import { HowToBuyModal } from '../HowToBuyModal';
+import {
+  Cog,
+  CurrentUserBadge,
+  CurrentUserBadgeMobile,
+} from '../CurrentUserBadge';
+import { ConnectButton } from '@oyster/common';
+import { MobileNavbar } from '../MobileNavbar';
+
+const getDefaultLinkActions = (connected: boolean) => {
+  return [
+    <Link to={`/`} key={'explore'}>
+      <Button className="app-btn">Explore</Button>
+    </Link>,
+    <Link to={`/collections`} key={'collections'}>
+      <Button className="app-btn">Collections</Button>
+    </Link>,
+    <Link to={`/artworks`} key={'artwork'}>
+      <Button className="app-btn">{connected ? 'My Items' : 'Artwork'}</Button>
+    </Link>,
+    <a target="_blank" href="https://bountyhunterspaceguild.com/">
+    <Button className="app-btn">Website</Button>
+  </a>,
+  <a target="_blank" href="https://magiceden.io/creators/bounty_hunter_space_guild">
+    <Button className="app-btn">Marketplace</Button>
+  </a>
+  ];
+};
+
+const DefaultActions = ({ vertical = false }: { vertical?: boolean }) => {
+  const { connected } = useWallet();
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: vertical ? 'column' : 'row',
+      }}
+    >
+      {getDefaultLinkActions(connected)}
+    </div>
+  );
+};
+
+export const MetaplexMenu = () => {
+  const { width } = useWindowDimensions();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const { connected } = useWallet();
+
+  if (width < 768)
+    return (
+      <>
+        <Modal
+          title={<img src={'/bhsg-logo.png'} />}
+          visible={isModalVisible}
+          footer={null}
+          className={'modal-box'}
+          closeIcon={
+            <img
+              onClick={() => setIsModalVisible(false)}
+              src={'/bhsg-logo.png'}
+            />
+          }
+        >
+          <div className="site-card-wrapper mobile-menu-modal">
+            <Menu onClick={() => setIsModalVisible(false)}>
+              {getDefaultLinkActions(connected).map((item, idx) => (
+                <Menu.Item key={idx}>{item}</Menu.Item>
+              ))}
+            </Menu>
+            <div className="actions">
+              {!connected ? (
+                <div className="actions-buttons">
+                  <ConnectButton
+                    onClick={() => setIsModalVisible(false)}
+                    className="secondary-btn"
+                  />
+                </div>
+              ) : (
+                <>
+                  <CurrentUserBadgeMobile
+                    showBalance={false}
+                    showAddress={true}
+                    iconSize={24}
+                    closeModal={() => {
+                      setIsModalVisible(false);
+                    }}
+                  />
+                  <Notifications />
+                  <Cog />
+                </>
+              )}
+            </div>
+          </div>
+        </Modal>
+        <MenuOutlined
+          onClick={() => setIsModalVisible(true)}
+          style={{ fontSize: '1.4rem' }}
+        />
+      </>
+    );
+
+  return <DefaultActions />;
+};
+
+export const LogoLink = () => {
+  return (
+    <Link to={`/`}>
+      <img src={'/bhsg-logo.png'} />
+    </Link>
+  );
+};
+
+export const AppBar = () => {
+  const { connected } = useWallet();
+  return (
+    <>
+      <MobileNavbar />
+      <div id="desktop-navbar">
+        <div className="app-left">
+          <LogoLink />
+          &nbsp;&nbsp;&nbsp;
+          <MetaplexMenu />
+        </div>
+        <div className="app-right">
+          {!connected && (
+            <ConnectButton style={{ height: 48 }} allowWalletChange />
+          )}
+          {connected && (
+            <>
+              <CurrentUserBadge
+                showBalance={false}
+                showAddress={true}
+                iconSize={24}
+              />
+              <Notifications />
+              <Cog />
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
